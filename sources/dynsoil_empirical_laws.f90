8d394068434382e2ce23d1f9764bb55bd002fff5
piermafrost <pierre.maffre@normalesup.org>
2020-06-24 18:05:46 -0700

    use double precision 'exp' and 'log' functions

--- sources/dynsoil_empirical_laws.f90
+++ sources/dynsoil_empirical_laws.f90
@@ -23,7 +23,7 @@ contains
     double precision, intent(in):: temp, runoff ! runoff (m/y) ; temperature (°C) !
     double precision:: reg_prod_opt
 !   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    reg_prod_opt = krp * runoff * exp(-(Ea_rp/Rgas)*(1./(temp+273.15)-1./T0_rp))
+    reg_prod_opt = krp * runoff * dexp(-(Ea_rp/Rgas)*(1./(temp+273.15)-1./T0_rp))
 !   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 !   Optimal soil production rate. Should be multiplied by the soil production function to get the actual soil production rate
   end function
@@ -36,7 +36,7 @@ contains
     double precision, intent(in):: h_soil
     double precision:: soil_prod_func
 !   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-!    soil_prod_func = ( exp(-1*h_soil/d1) - k1*exp(-1*h_soil/d2) ) / knorm ! normalized HUMPED FUNCTION
+!    soil_prod_func = ( dexp(-1*h_soil/d1) - k1*dexp(-1*h_soil/d2) ) / knorm ! normalized HUMPED FUNCTION
     soil_prod_func = h0/h_soil                                            ! inverse function
 !   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 !   Soil production function 
@@ -49,9 +49,9 @@ contains
     double precision, intent(in):: RPopt,E
     double precision:: eq_reg_thick
 !   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-!    eq_reg_thick = d1*log(RPopt/E) ! for humped function
+!    eq_reg_thick = d1*dlog(RPopt/E) ! for humped function
 !    eq_reg_thick = h0*RPopt/E      ! for inverse law
-    eq_reg_thick = max( h0*log(RPopt/E), 0.d+0 ) ! for exponential function
+    eq_reg_thick = max( h0*dlog(RPopt/E), 0.d+0 ) ! for exponential function
 !   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 !   Estimated soil equilibrium thickness by approximating the SPF.
   end function
@@ -63,7 +63,7 @@ contains
     double precision, intent(in):: temp, runoff ! runoff (m/y) ; temperature (°C) !
     double precision:: dissolution_constant
 !   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    dissolution_constant = kd * (1-exp(-kw*runoff)) * exp( (Ea/Rgas) * (1/T0-1/(temp+273.15)) )
+    dissolution_constant = kd * (1-dexp(-kw*runoff)) * dexp( (Ea/Rgas) * (1/T0-1/(temp+273.15)) )
 !   rock dissolution constant, computed from climatic variables
   end function
 
@@ -74,7 +74,7 @@ contains
     double precision, intent(in):: h, E, K
     double precision:: eq_x_p_surface
 !   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    eq_x_p_surface = exp( -1*K * ((h/E)**(sigma+1)) / (sigma+1) )
+    eq_x_p_surface = dexp( -1*K * ((h/E)**(sigma+1)) / (sigma+1) )
 !   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 !   Primary phases proportion at surface at steady-state
   end function
